1b407897c8426cc9145b531bfe7aca8ed4685429
colman423 <colman0423@gmail.com>
2018-02-05 23:17:58 +0800

    initial commit

---
+++ manager.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+import playScene as sc
+
+if __name__=="__main__" :
+    form = sc.initGUI()
+    form.mainloop()
---
+++ playScene.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from tkinter import *
+
+def initGUI():
+    form = Tk()
+    form.title("Mine Sweeper")
+
+
+    return form
+
